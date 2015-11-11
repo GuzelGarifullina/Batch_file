19d9694a8cf777d69726ed9e661fc01e2a077263
guzel <giselegarifullina@gmail.com>
2015-11-11 23:35:23 +0300

    Make solution

--- a.bat
+++ a.bat
@@ -1,27 +1,9 @@
+set pathMSBuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\"
 @echo off
+cls
+cd %pathMSBuild%
 
-if "%NETCF_PATH%" == "" ( set "NETCF_PATH=C:\Program Files\Microsoft.NET\SDK\CompactFramework\v3.5\WindowsCE" )
+set mypath=%~dp0
+msbuild.exe "%mypath%Hello.sln" /p:configuration=debug
 
-if DEFINED REF ( set REF= )
-
-set REF=%REF% "/r:%NETCF_PATH%\MsCorlib.dll"
-set REF=%REF% "/r:%NETCF_PATH%\System.Core.dll"
-set REF=%REF% "/r:%NETCF_PATH%\System.Data.dll"
-set REF=%REF% "/r:%NETCF_PATH%\System.dll"
-set REF=%REF% "/r:%NETCF_PATH%\System.Drawing.dll"
-set REF=%REF% "/r:%NETCF_PATH%\System.Messaging.dll"
-set REF=%REF% "/r:%NETCF_PATH%\System.Net.IrDA.dll"
-set REF=%REF% "/r:%NETCF_PATH%\System.Web.Services.dll"
-set REF=%REF% "/r:%NETCF_PATH%\System.Windows.Forms.dll"
-set REF=%REF% "/r:%NETCF_PATH%\System.ServiceModel.dll"
-set REF=%REF% "/r:%NETCF_PATH%\System.Runtime.Serialization.dll"
-set REF=%REF% "/r:%NETCF_PATH%\System.Xml.dll"
-set REF=%REF% "/r:%NETCF_PATH%\System.Xml.Linq.dll"
-set REF=%REF% "/r:%NETCF_PATH%\Microsoft.WindowsCE.Forms.dll"
-set REF=%REF% "/r:%NETCF_PATH%\Microsoft.WindowsMobile.DirectX.dll"
-set REF=%REF% "/r:%NETCF_PATH%\Microsoft.ServiceModel.Channels.Mail.dll"
-set REF=%REF% "/r:%NETCF_PATH%\Microsoft.ServiceModel.Channels.Mail.WindowsMobile.dll"
-
-rem :compile
-
-csc -nostdlib -noconfig %REF% %*
+pause

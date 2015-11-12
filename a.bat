3d2c6137572af6fe8e1df94e44fe6be8cd075589
guzel <giselegarifullina@gmail.com>
2015-11-12 20:39:34 +0300

    Deleted junk lines

--- a.bat
+++ a.bat
@@ -2,7 +2,7 @@ set pathMSBuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\"
 @echo off
 cls
 cd %pathMSBuild%
-<<<<<<< HEAD
+
 set mypath=%~dp0
 set repo=Hello\
 
@@ -26,12 +26,5 @@ echo New build >> buildHistory.txt
 echo %DATE%  >> buildHistory.txt
 echo %TIME%  >> buildHistory.txt
 msbuild.exe "%mypath%Hello.sln" /p:configuration=debug   >> buildHistory.txt
-
-
-=======
-
-set mypath=%~dp0
-msbuild.exe "%mypath%Hello.sln" /p:configuration=debug
->>>>>>> 19d9694a8cf777d69726ed9e661fc01e2a077263
 
 pause
